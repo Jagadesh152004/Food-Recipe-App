8c4fb7620211ce5db0c0885ef96966d0eaf48d12
Jagadesh152004 <jagadeshwaran152004@gmail.com>
2025-11-17 20:50:23 +0530

    Added the all the project files

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCoffee, faHeart, faCode } from "@fortawesome/free-solid-svg-icons";
+
+function Footer() {
+    return (
+        <div className="bg-background-tertiary text-copy-primary flex flex-row sm:flex-col sm:text-center sm:justify-around justify-between bg-gray-800 h-32">
+            <div className="container flex flex-col justify-center ml-12 sm:ml-0">
+                <p className="text-gray-100 text-sm sm:text-xs mb-1">
+                    Powered by MealDB
+                </p>
+                <p className="text-gray-500 text-lg sm:text-sm">
+                    Proudly built with ⚛
+                </p>
+            </div>
+            
+        </div>
+    );
+}
+
+export default Footer;
