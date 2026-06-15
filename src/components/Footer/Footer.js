85598caa7fba80dfc1e88385ad1387f2bb3370a9
Jagadesh152004 <jagadeshwaran152004@gmail.com>
2026-06-15 14:22:59 +0530

    Remove unused FontAwesome imports from Footer component

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,6 +1,4 @@
 import React from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCoffee, faHeart, faCode } from "@fortawesome/free-solid-svg-icons";
 
 function Footer() {
     return (
